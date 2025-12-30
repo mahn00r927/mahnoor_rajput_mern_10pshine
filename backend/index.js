bbd8de1a151363a01e93d3e6cb006ffd914af3bf
Mahnoor Rajput <mahnoor.rajput11927@gmail.com>
2025-12-30 17:35:43 +0500

    DB of Users

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
+
 const express = require('express');
+const mongoose = require("mongoose");
 const cors = require('cors');
 require('dotenv').config();
 
+const authRoutes = require("./Routes/Auth");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Server is running');
@@ -12,3 +20,11 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch((err) => {
+    console.error("❌ Connection Error Type:", err.name);
+    console.error("❌ Error Message:", err.message);
+  });
