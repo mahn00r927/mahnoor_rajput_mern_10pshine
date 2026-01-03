5533a3130232f5341130325443ef5c657dce6aea
Mahnoor Rajput <mahnoor.rajput11927@gmail.com>
2026-01-03 14:11:56 +0500

    Add Logger

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const noteRoutes = require("./Routes/Notes");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,6 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// const noteRoutes = require("./Routes/Notes");
 
-// app.use("/api/notes", noteRoutes);
+
+app.use("/api/notes", noteRoutes);
