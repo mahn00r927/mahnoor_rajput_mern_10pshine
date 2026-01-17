ba330db193ad58fae42f8d89203681713833d9aa
Mahnoor Rajput <mahnoor.rajput11927@gmail.com>
2026-01-17 15:52:37 +0500

    Set Some Margins

--- frontend/src/UserDashboard/Dashboard.tsx
+++ frontend/src/UserDashboard/Dashboard.tsx
@@ -113,7 +113,7 @@ export default function Dashboard() {
 
 
   return (
-    <div className="flex h-180 bg-gray-950 text-white">
+    <div className="flex h-190 bg-gray-950 text-white">
       <Sidebar
         onNewNote={handleNewNote}
         folders={uniqueFolders}
